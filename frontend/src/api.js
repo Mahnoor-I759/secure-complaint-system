5c493a584b464bd86ddb9a0c4d6a02bdc5b2cd7f
Mahnoor <241548@students.au.edu.pk>
2026-05-25 18:05:37 +0500

    connect to live backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'https://your-railway-url.up.railway.app',
 });
 
 API.interceptors.request.use((config) => {
